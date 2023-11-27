c9a3082ba1ee2c7c749c668c75c00a6647020225
Kevin Cai <k1029384756c@gmail.com>
2023-11-27 07:37:50 -0500

    Zero out buffer before reading

--- src/fuse_cpp_ramfs.cpp
+++ src/fuse_cpp_ramfs.cpp
@@ -636,7 +636,7 @@ void FuseRamFs::FuseReadDir(fuse_req_t req, fuse_ino_t ino, size_t size,
 
     // Pick the lesser of the max response size or our max size.
     size_t bufSize = FuseRamFs::kReadDirBufSize < size ? FuseRamFs::kReadDirBufSize : size;
-    char *buf = (char *) malloc(bufSize);
+    char *buf = (char *) calloc(bufSize,sizeof(char));
     if (buf == nullptr) {
         fuse_reply_err(req, ENOMEM);
     }
@@ -675,7 +675,6 @@ void FuseRamFs::FuseReadDir(fuse_req_t req, fuse_ino_t ino, size_t size,
                                         ++ctx->cookie);
         if (bytesAdded > bufSize) {
             // Oops. There wasn't enough space for that last item. Back up and exit.
-            // --(ctx->it);
             bytesAdded = oldSize;
             break;
         } else {
